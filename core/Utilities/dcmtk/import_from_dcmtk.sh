02e79409700c7a97742c56d066aed610d7cffe5c
mphasak <mphasak@42a5c34f-2066-0410-bec5-ba365beb4995>
2009-07-13 18:29:27 +0000

    FIX: DCMTK import script uses cmdline arg for source directory
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@188 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/Utilities/dcmtk/import_from_dcmtk.sh
+++ core/Utilities/dcmtk/import_from_dcmtk.sh
@@ -4,7 +4,6 @@ DCMTK_ROOT=$1
 
 files="COPYRIGHT"
 
-DCM_SOURCE_ROOT="/Users/mphasak/Downloads/dcmtk-3.5.4"
 CMTK_ROOT="/Users/mphasak/Development/cmtk/trunk/core"
 CMTK_DCMTK_ROOT="${CMTK_ROOT}/Utilities/dcmtk"
 CMTK_DCMTK_INCLUDE="${CMTK_ROOT}/Utilities/dcmtk/dcmtk"
@@ -13,7 +12,7 @@ CONFIG_DIR="config"
 CONFIG_MODULES="cfwin32 osconfig"
 
 DCMDATA_DIR="dcmdata"
-DCMDATA_MODULES="cmdlnarg dcbytstr dcchrstr dccodec dcdatset dcdebug dcdefine dcdeftag dcdicdir dcdicent dcdict dcdirrec dcelem dcerror dcfilefo dchashdi dcistrma dcistrmf dcitem dclist dcmetinf dcobject dcofsetl dcostrma dcostrmf dcovlay dcpcache dcpixel dcpixseq dcpxitem dcsequen dcstack dcswap dctag dctagkey dctk dctypes dcuid dcvm dcvr dcvrae dcvras dcvrat dcvrcs dcvrda dcvrds dcvrdt dcvrfd dcvrfl dcvris dcvrlo dcvrlt dcvrobow dcvrof dcvrpn dcvrpobw dcvrsh dcvrsl dcvrss dcvrst dcvrtm dcvrui dcvrul dcvrulup dcvrus dcvrut dcxfer"
+DCMDATA_MODULES="cmdlnarg dcbytstr dcchrstr dccodec dcdatset dcdebug dcdefine dcdeftag dcdicdir dcdicent dcdict dcdirrec dcelem dcerror dcfilefo dchashdi dcistrma dcistrmf dcitem dclist dcmetinf dcobject dcofsetl dcostrma dcostrmf dcostrmz dcovlay dcpcache dcpixel dcpixseq dcpxitem dcsequen dcstack dcswap dctag dctagkey dctk dctypes dcuid dcvm dcvr dcvrae dcvras dcvrat dcvrcs dcvrda dcvrds dcvrdt dcvrfd dcvrfl dcvris dcvrlo dcvrlt dcvrobow dcvrof dcvrpn dcvrpobw dcvrsh dcvrsl dcvrss dcvrst dcvrtm dcvrui dcvrul dcvrulup dcvrus dcvrut dcxfer"
 DCMIMGLE_DIR="dcmimgle"
 DCMIMGLE_MODULES="didocu diobjcou diutils"
 
@@ -28,11 +27,11 @@ copy_src_tgt()
   local module_dir=$1
   local module_list=$*
 
-  src_h_path=${DCM_SOURCE_ROOT}/${module_dir}/include/dcmtk/${module_dir}
+  src_h_path=${DCMTK_ROOT}/${module_dir}/include/dcmtk/${module_dir}
   tgt_h_path=${CMTK_DCMTK_INCLUDE}/${module_dir}
   mkdir -p ${tgt_h_path}
   
-  src_c_path=${DCM_SOURCE_ROOT}/${module_dir}/libsrc
+  src_c_path=${DCMTK_ROOT}/${module_dir}/libsrc
   #tgt_c_path=${CMTK_DCMTK_ROOT}/src/${module_dir}
   tgt_c_path=${CMTK_DCMTK_ROOT}
   mkdir -p ${tgt_c_path}
@@ -49,6 +48,6 @@ copy_src_tgt ${DCMDATA_DIR} ${DCMDATA_MODULES};
 copy_src_tgt ${DCMIMGLE_DIR} ${DCMIMGLE_MODULES};
 copy_src_tgt ${DCMJPEG_DIR} ${DCMJPEG_MODULES};
 copy_src_tgt ${OFSTD_DIR} ${OFSTD_MODULES};
-cp ${DCM_SOURCE_ROOT}/dcmdata/libsrc/dicom.dic ${CMTK_DCMTK_ROOT}
+cp ${DCMTK_ROOT}/dcmdata/libsrc/dicom.dic ${CMTK_DCMTK_ROOT}
 
 
