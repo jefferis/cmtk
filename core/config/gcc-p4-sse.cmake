f2080d98f99861fc7ce8f61b3780865a466acbed
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2009-07-27 18:45:52 +0000

    IMP: condensed P4 build parameters
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@293 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/config/gcc-p4-sse.cmake
+++ core/config/gcc-p4-sse.cmake
@@ -1,2 +1,2 @@
-SET(CMAKE_CXX_FLAGS "-m32 -march=native -mmmx -msse -msse2 -mfpmath=sse")
+SET(CMAKE_CXX_FLAGS "-march=pentium4 -mmmx -msse -msse2 -mfpmath=sse")
 SET(CMAKE_C_FLAGS ${CMAKE_CXX_FLAGS})
