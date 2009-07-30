4d33db7bf5db5756775c93d92a265ab6e6389432
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2009-07-30 21:28:09 +0000

    ADD: asegment_sri24 tool if SRI24 atlas is supported
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@332 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/apps/asegment_sri24.cxx
@@ -0,0 +1,118 @@
+/*
+//
+//  Copyright 1997-2009 Torsten Rohlfing
+//  Copyright 2004-2009 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision: 326 $
+//
+//  $LastChangedDate: 2009-07-29 15:08:42 -0700 (Wed, 29 Jul 2009) $
+//
+//  $LastChangedBy: torstenrohlfing $
+//
+*/
+
+#include <cmtkconfig.h>
+
+#include <cmtkCommandLine.h>
+#include <cmtkConsole.h>
+
+#include <list>
+#include <string>
+
+#include <cmtkVolumeIO.h>
+#include <cmtkAtlasSegmentation.h>
+
+int
+main( const int argc, const char* argv[] )
+{
+  bool verbose = false;
+  bool fast = false;
+  
+  const char* targetImageName = NULL;
+  const char* outImageName = NULL;
+
+  std::string channelSRI24( "spgr" );
+  std::string labelsSRI24( "tzo116plus" );
+  
+  try
+    {
+    cmtk::CommandLine  cl( argc, argv, cmtk::CommandLine::PROPS_XML );
+    cl.SetProgramInfo( cmtk::CommandLine::PRG_TITLE, "Segmentation using SRI24 atlas" );
+    cl.SetProgramInfo( cmtk::CommandLine::PRG_DESCR, "Register a target image to a selected channel of the SRI24 atlas, then reformat one of the atlas label maps to the target image." );
+    cl.SetProgramInfo( cmtk::CommandLine::PRG_CATEG, "CMTK.Segmentation" );
+
+    typedef cmtk::CommandLine::Key Key;
+    cl.AddSwitch( Key( 'v', "verbose" ), &verbose, true, "Verbose mode." );
+    cl.AddSwitch( Key( 'f', "fast" ), &fast, true, "Fast mode." );
+
+    cmtk::CommandLine::EnumGroup<std::string>::SmartPtr channelGroup = cl.AddEnum( "RegistrationChannel", &channelSRI24, "The SRI24 channel used for registration to the target image." );
+    channelGroup->AddSwitch( Key( "spgr" ), "spgr", "SPGR (T1-weighted) structural channel" );
+    channelGroup->AddSwitch( Key( "early-fse" ), "erly", "Early-echo (PD-weighted) fast spin echo channel" );
+    channelGroup->AddSwitch( Key( "late-fse" ), "late", "Late-echo (T2-weighted) fast spin echo channel" );
+    channelGroup->AddSwitch( Key( "fa" ), "fa", "Fractional anisotropy channel, derived from diffusion tensor images" );
+    
+    cmtk::CommandLine::EnumGroup<std::string>::SmartPtr labelsGroup = cl.AddEnum( "ReformatLabelMap", &labelsSRI24, "The SRI24 label map that is reformatted to the target image." );
+    channelGroup->AddSwitch( Key( "tzo116plus" ), "tzo116plus", "Extended cortical parcellation template based on Tzourio-Mazoyer 116 region template." );
+    channelGroup->AddSwitch( Key( "lpba40" ), "lpba40", "Template based on the 40 subject LONI Probabilistic Brain Atlas segmentation." );
+    channelGroup->AddSwitch( Key( "tissue" ), "tissue", "SRI24 maximum likelihood three compartment (GM, WM, CSF) tissue segmentation map." );
+    
+    cl.AddParameter( &targetImageName, "TargetImage", "Target image path. This is the image to be segmented." )->SetProperties( cmtk::CommandLine::PROPS_IMAGE );
+    cl.AddParameter( &outImageName, "OutputImage", "Output image path. This is where the reformatted label map is written." )->SetProperties( cmtk::CommandLine::PROPS_IMAGE | cmtk::CommandLine::PROPS_OUTPUT );
+
+    cl.Parse();
+    }
+  catch ( cmtk::CommandLine::Exception e )
+    {
+    cmtk::StdErr << e << "\n";
+    exit( 1 );
+    }
+  
+  cmtk::UniformVolume::SmartPtr targetImg( cmtk::VolumeIO::ReadOriented( targetImageName, verbose ) );
+  if ( !targetImg ) 
+    {
+    cmtk::StdErr << "ERROR: could not read target image " << targetImageName << "\n";
+    exit( 1 );
+    }
+  
+  std::string atlasImageName = std::string( CMTK_ROOT_PATH_SRI24 ) + "/" + "spgr" + "nii";
+  cmtk::UniformVolume::SmartPtr atlasImg( cmtk::VolumeIO::ReadOriented( atlasImageName.c_str(), verbose ) );
+  if ( !atlasImg ) 
+    {
+    cmtk::StdErr << "ERROR: could not read atlas image " << atlasImageName << "\n";
+    exit( 1 );
+    }
+  
+  std::string atlasLabelName = std::string( CMTK_ROOT_PATH_SRI24 ) + "/" + "tzo116plus" + "nii";
+  cmtk::UniformVolume::SmartPtr atlasLbl( cmtk::VolumeIO::ReadOriented( atlasLabelName.c_str(), verbose ) );
+  if ( !atlasLbl ) 
+    {
+    cmtk::StdErr << "ERROR: could not read atlas labels " << atlasLabelName << "\n";
+    exit( 1 );
+    }
+    
+  cmtk::AtlasSegmentation segment( targetImg, atlasImg, atlasLbl );
+  segment.SetVerbose( verbose );
+  segment.SetFast( fast );
+
+  cmtk::VolumeIO::Write( segment.GetLabelMap(), outImageName );
+
+  return 0;
+}
