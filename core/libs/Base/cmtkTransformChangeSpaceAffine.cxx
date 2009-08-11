275dc2217f0085717f4db9d526e7852a5c3bc06a
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2009-08-11 23:26:59 +0000

    CHG: moved class to Base because we need it in IO also
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@453 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/libs/Base/cmtkTransformChangeSpaceAffine.cxx
@@ -0,0 +1,56 @@
+/*
+//
+//  Copyright 1997-2009 Torsten Rohlfing
+//  Copyright 2004-2009 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#include <cmtkTransformChangeSpaceAffine.h>
+
+void
+cmtk::TransformChangeSpaceAffine
+::ComputeNewTransformation
+( const AffineXform* xform, const UniformVolume* reference, const std::string& referenceSpaceNew, const UniformVolume* floating, const std::string& floatingSpaceNew )
+{
+  // adapt transformation to Slicer's image coordinate systems as defined in the Nrrd files we probably read
+  UniformVolume::SmartPtr refVolumeOriginalSpace( reference->CloneGrid() );
+  UniformVolume::SmartPtr fltVolumeOriginalSpace( floating->CloneGrid() );
+  
+  // first bring volumes back into their native coordinate space.
+  refVolumeOriginalSpace->ChangeCoordinateSpace( referenceSpaceNew );
+  fltVolumeOriginalSpace->ChangeCoordinateSpace( floatingSpaceNew );
+  
+  // now determine image-to-physical transformations and concatenate these.
+  const AffineXform::MatrixType refMatrix = refVolumeOriginalSpace->GetImageToPhysicalMatrix ();
+  const AffineXform::MatrixType fltMatrix = fltVolumeOriginalSpace->GetImageToPhysicalMatrix ();
+  
+  AffineXform::MatrixType concatMatrix = refMatrix;
+  (concatMatrix.Invert() *= xform->Matrix) *= fltMatrix;
+  
+  // create output transformation and write
+  this->m_NewXform.SetMatrix( concatMatrix );
+}
