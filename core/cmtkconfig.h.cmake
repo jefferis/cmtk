b814e7cb11d82c52e256c570807ab1495ae022fe
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2009-08-26 21:56:03 +0000

    FIX: VS pragma
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@519 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/cmtkconfig.h.cmake
+++ core/cmtkconfig.h.cmake
@@ -98,7 +98,7 @@
 // disable warnings about insecure functions (we want to be portable)
 #  define _CRT_SECURE_NO_DEPRECATE
 // disable warnings about unknown (i.e., gcc) pragmas
-#  pragma warning disable 4068
+#  pragma warning ( disable: 4068 )
 // enable POSIX compliance
 #  define _POSIX_
 #  define NOMINMAX
