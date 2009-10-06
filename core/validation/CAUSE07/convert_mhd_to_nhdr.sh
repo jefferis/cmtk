fa76f26b9a15542131eebed20c69b6a524b16575
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2009-10-06 21:41:07 +0000

    ADD: conversion script for CAUSE07 data
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@549 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/validation/CAUSE07/convert_mhd_to_nhdr.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#
+# This script extracts geometry information from a MetaImage (mhd) header
+# file and creates a matching NRRD header (nhdr) file.
+#
+
+#
+# IMPORTANT:
+#
+# The CAUSE07 images are stored in RAI space, which NRRD does not support. So
+# We store in RAS space instead with negated z direction.
+#
+
+in_mhd=$1
+out_nhdr=$2
+
+NDIMS=`fgrep DimSize ${in_mhd} | sed 's/.* = //g'`
+
+SPACING=`fgrep ElementSpacing ${in_mhd} | sed 's/.* = //g'`
+DIR1=`echo ${SPACING} | cut -d" " -f1`
+DIR2=`echo ${SPACING} | cut -d" " -f2`
+DIR3=`echo ${SPACING} | cut -d" " -f3`
+DIRECTIONS="(${DIR1},0,0) (0,${DIR2},0) (0,-${DIR3},0)"
+
+FILE=`fgrep ElementDataFile ${in_mhd} | sed 's/.* = //g'`
+
+echo "NRRD0004
+# Complete NRRD file format specification at:
+# http://teem.sourceforge.net/nrrd/format.html
+type: short
+dimension: 3
+space: right-anterior-superior
+sizes: ${NDIMS}
+space directions: ${DIRECTIONS}
+kinds: domain domain domain
+endian: little
+encoding: gzip
+space origin: (0,0,0)
+data file: ${FILE}.gz" > ${out_nhdr}
