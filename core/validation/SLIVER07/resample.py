32ecb0c88be9104d89de46c632cc4dfb7cd9b27f
mphasak <mphasak@42a5c34f-2066-0410-bec5-ba365beb4995>
2009-11-06 22:49:35 +0000

    UPD: refactored to allow arbitrary resampling target size
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@731 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/validation/SLIVER07/resample.py
+++ core/validation/SLIVER07/resample.py
@@ -2,7 +2,8 @@
 
 import os
 
-appRoot = '/Users/mphasak/Development/cmtk/cur/build/bin/'
+appRoot = '/Users/mphasak/Development/cmtk/cur/build/bin'
+sliverRoot = '/Users/mphasak/Development/sliver07'
 
 convert = os.path.join(appRoot,"convert")
 reformatx = os.path.join(appRoot,"reformatx")
@@ -14,34 +15,42 @@ ids = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15
 # These two volumes didn't resample well so leave them out for now
 badResamples = ['14','20']
 
-# Factors for converting the Z thickness to 30
-reformat30factors = [6.0999999999999996, 2.1333333333333333, 2.6333333333333333, 7.0666666666666664, 10.633333333333333, 3.7000000000000002, 8.3666666666666671, 7.5999999999999996, 7.0, 6.3666666666666663, 12.933333333333334, 7.333333333333333, 4.833333333333333, 4.2999999999999998, 13.133333333333333, 5.0333333333333332, 4.0333333333333332, 8.1666666666666661, 11.166666666666666, 6.0999999999999996]
+# number of Z voxels in each volume
+zdims = [183,64,79,212,319,111,251,228,210,191,388,220,145,129,394,151,121,245,335,183]
 
-
-def reformat30():
+def reformat(to):
   inDir = "training"
-  gridDir = "grid30"
-  outDir = "resamp30"
+  gridDir = "grid" + str(to)
+  outDir = "resamp" + str(to)
+  
   for i in range(0,len(ids)):
     id = ids[i]
     if id not in badResamples:
-      divBy = str(reformat30factors[i])
       
-      infile = os.path.join(inDir, "liver-orig0" + id + ".nhdr")
-      gridfile = os.path.join(gridDir,"liver-orig0" + id + "-30grid.nhdr")
-      outfile = os.path.join(outDir,"liver-orig0" + id + "-30.nhdr")
-      
-      cmd_mk_grid = mk_analyze_hdr + " -D 30,30,30 -V 17.06,17.06," + divBy +\
-                            " " +  gridfile
+      infile = os.path.join(sliverRoot, inDir, "liver-orig0" + id + ".nhdr")
+      gridfile = os.path.join(sliverRoot, gridDir,"liver-orig0" + id + "-30grid.nhdr")
+      outfile = os.path.join(sliverRoot, outDir,"liver-orig0" + id + "-30.nhdr")
+     
+      divXBy = divYBy = 512 / float(to)
+      divZBy = zdims[i] / float(to) 
+     
+      # % here is like sprintf in C
+      newGridSize = "%f,%f,%f" % (to, to, to)  # voxels
+      newVolumeSizeMM = "%f,%f,%f" % (divXBy, divYBy, divZBy) # mm
 
-      cmd_reformat = reformatx + " -o " + outfile +\
-                        " --floating " + infile +\
-                        " " + gridfile
+      cmd_mk_grid = ( mk_analyze_hdr + 
+                      " -D " + newGridSize +
+                      " -V " + newVolumeSizeMM + 
+                      " " +  gridfile )
+
+      cmd_reformat = ( reformatx + " -o " + outfile +
+                       " --floating " + infile +
+                       " " + gridfile )
 
       print cmd_mk_grid
       print cmd_reformat
       
-      os.system(cmd_mk_grid)
-      os.system(cmd_reformat)
+      #os.system(cmd_mk_grid)
+      #os.system(cmd_reformat)
 
-reformat30()
+#reformat(30)
