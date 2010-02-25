bea396db342bd5e125eb38888785dced91c9460f
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-02-25 22:53:25 +0000

    ADD: skeleton wrapper class for SQLite
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@1241 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/libs/IO/cmtkSQLite.cxx
@@ -0,0 +1,63 @@
+/*
+//
+//  Copyright 2010 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#include <cmtkSQLite.h>
+
+#include <iostream>
+
+cmtk::SQLite::SQLite
+( const std::string& dbPath, const bool readOnly )
+{
+  if ( readOnly )
+    {
+    const int rc = sqlite3_open_v2( dbPath, &this->m_DB, SQLITE_OPEN_READONLY, NULL /*zVFS*/ );
+    if( rc )
+      {
+      cmtk::StdErr << "Can't open database: " << sqlite3_errmsg( this->m_DB ) << "\n";
+      sqlite3_close( this->m_DB );
+      exit(1);
+      }
+    }
+  else
+    {
+    const int rc = sqlite3_open_v2( dbPath, &this->m_DB, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE. NULL, /*zVFS*/ );
+    if( rc )
+      {
+      cmtk::StdErr << "Can't open database: " << sqlite3_errmsg( this->m_DB ) << "\n";
+      sqlite3_close( this->m_DB );
+      exit(1);
+      }
+    }
+}
+
+cmtk::SQLite::~SQLite()
+{
+  sqlite3_close( this->m_DB );
+}
---
+++ core/libs/IO/cmtkSQLite.h
@@ -0,0 +1,67 @@
+/*
+//
+//  Copyright 2010 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#ifndef __cmtkSQLite_h_included_
+#define __cmtkSQLite_h_included_
+
+#include <cmtkconfig.h>
+
+#include <sqlite3.h>
+#include <string>
+
+namespace
+cmtk
+{
+
+/** \addtogroup IO */
+//@{
+
+/** Wrapper class for SQLite database.
+ */
+class SQLite
+{
+public:
+  /// Constructor: open SQLite database.
+  SQLite( const std::string& dbPath, /**!< Path to the SQLite3 database file. */
+	  const bool readOnly = false /**!< If this flag is set, the database is opened read-only. If false, the database is opened for read/write, and a non-existing database will be created. */);
+  
+  /// Destructor: close database.
+  virtual ~SQLite();
+
+private:
+  /// Database object.
+  sqlite3 *m_DB;
+};
+
+//@}
+
+} // namespace cmtk
+
+#endif // #ifndef __cmtkSQLite_h_included_
