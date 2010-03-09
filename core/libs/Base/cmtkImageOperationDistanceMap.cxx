b869de7f4a8b5eed3735221f63a7cd557665a6db
torsten_at_home <torsten_at_home@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-03-09 23:54:15 +0000

    ADD: distance map image operation
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@1306 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/libs/Base/cmtkImageOperationDistanceMap.cxx
@@ -0,0 +1,56 @@
+/*
+//
+//  Copyright 2009,2010 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#include <cmtkImageOperationDistanceMap.h>
+
+cmtk::UniformVolume::SmartPtr  
+cmtk::ImageOperationDistanceMap
+::Apply( cmtk::UniformVolume::SmartPtr& volume )
+{
+  if ( this->m_SignedDistance )
+    {
+    UniformVolume::SmartPtr iMap( new DistanceMapType( volume, DistanceMapType::INSIDE ) );
+    UniformVolume::SmartPtr oMap( new DistanceMapType( volume ) );
+    
+    const size_t nPixels = volume->GetNumberOfPixels();
+#pragma omp parallel for
+    for ( size_t n = 0; n < nPixels; ++n )
+      {
+      Types::DataItem iValue = iMap->GetDataAt( n );
+      if ( iValue > 0 )
+	oMap->SetDataAt( iValue, n );
+      }
+    return oMap;
+    }
+  else
+    {
+    return UniformVolume::SmartPtr( new DistanceMapType( volume ) );
+    }
+}
---
+++ core/libs/Base/cmtkImageOperationDistanceMap.h
@@ -0,0 +1,77 @@
+/*
+//
+//  Copyright 2009,2010 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#ifndef __cmtkImageOperationDistanceMap_h_included_
+#define __cmtkImageOperationDistanceMap_h_included_
+
+#include <cmtkconfig.h>
+
+#include <cmtkImageOperation.h>
+#include <cmtkUniformDistanceMap.h>
+
+namespace
+cmtk
+{
+
+/// Compute distance map.
+class ImageOperationDistanceMap
+/// Inherit generic image operation.
+  : public ImageOperation
+{
+public:
+  /// Distance map type.
+  typedef UniformDistanceMap<double> DistanceMapType;
+
+  /// Constructor.
+  ImageOperationDistanceMap( const bool signedDistance ) : m_SignedDistance( signedDistance ) {}
+
+  /// Apply this operation to an image in place.
+  virtual UniformVolume::SmartPtr Apply( UniformVolume::SmartPtr& volume );
+
+  /// Create new signed distance map operation.
+  static void NewSigned( const char* maskFileName )
+  {
+    ImageOperation::m_ImageOperationList.push_back( SmartPtr( new ImageOperationDistanceMap( true /*signedDistance*/) ) );
+  }
+
+  /// Create new unsigned distance map operation.
+  static void NewUnsigned( const char* maskFileName )
+  {
+    ImageOperation::m_ImageOperationList.push_back( SmartPtr( new ImageOperationDistanceMap( false /*signedDistance*/) ) );
+  }
+  
+private:
+  /// Flag for signed (inside/outside) vs. unsigned (outside only) distance map.
+  bool m_SignedDistance;
+};
+
+} // namespace cmtk
+
+#endif // #ifndef __cmtkImageOperationDistanceMap_h_included_
