26362b5168d8f734afb0ad1c893acb4b26d3d609
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-03-15 18:34:30 +0000

    FIX: inverse mask was broken.
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@1346 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/Base/cmtkImageOperationApplyMask.h
+++ core/libs/Base/cmtkImageOperationApplyMask.h
@@ -45,48 +45,25 @@ class ImageOperationApplyMask
   : public ImageOperation
 {
 public:
+  /// This class.
+  typedef ImageOperationApplyMask Self;
+
   /// Constructor.
   ImageOperationApplyMask( const cmtk::UniformVolume::SmartPtr& maskVolume ) : m_MaskVolume( maskVolume ) {}
 
   /// Apply this operation to an image in place.
-  virtual cmtk::UniformVolume::SmartPtr  Apply( cmtk::UniformVolume::SmartPtr& volume )
-  {
-    const std::string maskOrientation = this->m_MaskVolume->m_MetaInformation[CMTK_META_IMAGE_ORIENTATION];
-    const std::string workingOrientation = volume->m_MetaInformation[CMTK_META_IMAGE_ORIENTATION];
-    if ( maskOrientation != workingOrientation )
-      {
-      this->m_MaskVolume = cmtk::UniformVolume::SmartPtr( this->m_MaskVolume->GetReoriented( workingOrientation.c_str() ) );
-      }
-    
-    for ( int dim = 0; dim < 3; ++dim )
-      {
-      if ( this->m_MaskVolume->m_Dims[dim] != volume->m_Dims[dim] )
-	{
-	cmtk::StdErr << "ERROR: mask volume dimensions do not match working volume dimensions.\n";
-	exit( 1 );
-	}
-      }
-    
-    const cmtk::TypedArray* maskData = this->m_MaskVolume->GetData();
-    cmtk::TypedArray::SmartPtr& volumeData = volume->GetData();
-
-    const size_t nPixels = volume->GetNumberOfPixels();
-    for ( size_t i = 0; i < nPixels; ++i )
-      if ( maskData->IsPaddingOrZeroAt( i ) ) 
-	volumeData->SetPaddingAt( i );
-    return volume;
-  }
+  virtual cmtk::UniformVolume::SmartPtr  Apply( cmtk::UniformVolume::SmartPtr& volume );
 
   /// Create new mask operation.
   static void New( const char* maskFileName )
   {
-    ImageOperation::m_ImageOperationList.push_back( SmartPtr( new ImageOperationApplyMask( ReadMaskFile( maskFileName ) ) ) );
+    ImageOperation::m_ImageOperationList.push_back( SmartPtr( new ImageOperationApplyMask( Self::ReadMaskFile( maskFileName ) ) ) );
   }
 
   /// Create new inverse mask operation.
   static void NewInverse( const char* maskFileName )
   {
-    ImageOperation::m_ImageOperationList.push_back( SmartPtr( new ImageOperationApplyMask( ReadMaskFile( maskFileName, true /*inverse*/ ) ) ) );
+    ImageOperation::m_ImageOperationList.push_back( SmartPtr( new ImageOperationApplyMask( Self::ReadMaskFile( maskFileName, true /*inverse*/ ) ) ) );
   }
 
 private:
@@ -94,29 +71,7 @@ private:
   cmtk::UniformVolume::SmartPtr m_MaskVolume;
 
   /// Read the actual mask file.
-  static cmtk::UniformVolume::SmartPtr ReadMaskFile( const char* maskFileName, const bool inverse = false )
-  {
-    cmtk::UniformVolume::SmartPtr maskVolume( cmtk::VolumeIO::ReadOriented( maskFileName ) );
-    if ( !maskVolume || !maskVolume->GetData() ) 
-      {
-      cmtk::StdErr << "ERROR: could not read mask from file " << maskFileName << "\nProgram will terminate now, just to be safe.\n";
-      exit( 1 );
-      }
-    
-    // binarize mask to 1/0, convert to char, and also consider "inverse" flag in the process.
-    cmtk::TypedArray::SmartPtr& maskData = maskVolume->GetData();
-    const size_t nPixels = maskData->GetDataSize();
-    for ( size_t n = 0; n < nPixels; ++n )
-      {
-      if ( maskData->IsPaddingOrZeroAt( n ) != inverse ) 
-	maskData->Set( n, 1 );
-      else
-	maskData->Set( n, 0 );
-      }
-    maskVolume->SetData( cmtk::TypedArray::SmartPtr( maskData->Convert( cmtk::TYPE_BYTE ) ) );
-    
-    return maskVolume;
-  }
+  static cmtk::UniformVolume::SmartPtr ReadMaskFile( const char* maskFileName, const bool inverse = false );
 };
 
 } // namespace cmtk
