26c23da89ee1396c261abacfea3c852edb51787e
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-04-01 23:07:10 +0000

    FIX: use C++ headers
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@1404 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/System/cmtkSmartPtr.h
+++ core/libs/System/cmtkSmartPtr.h
@@ -1,7 +1,7 @@
 /*
 //
 //  Copyright 1997-2009 Torsten Rohlfing
-//  Copyright 2004-2009 SRI International
+//  Copyright 2004-2010 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
@@ -39,9 +39,9 @@
 #endif
 
 #ifdef DEBUG
-#  include <stdio.h>
+#  include <cstdio>
 #endif
-#include <assert.h>
+#include <cassert>
 
 #include <cmtkSafeCounter.h>
 
