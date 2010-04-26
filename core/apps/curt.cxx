c83890e0bd62d9274106bbe95caa64b6ed39cb11
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-04-26 23:17:22 +0000

    ## ADD: collapsing of constant reference ranges
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@1556 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/apps/curt.cxx
+++ core/apps/curt.cxx
@@ -72,6 +72,7 @@ main( const int argc, const char* argv[] )
   std::vector<std::string> pathsLbls;
 
   bool padZero = false;
+  bool collapse = false;
 
   try
     {
@@ -85,6 +86,7 @@ main( const int argc, const char* argv[] )
 
     cl.BeginGroup( "Preprocessing", "Input Image Preprocessing" );
     cl.AddSwitch( Key( "pad" ), &padZero, true, "Pad (ignore) zero-filled areas in both images" );
+    cl.AddSwitch( Key( "collapse" ), &collapse, true, "Collapse transformation for equal reference image values to the same floating image pixel." );
 //    cl.AddOption( Key( 'm', "mask" ), &FNameMaskImage, "Binary mask image filename." )->SetProperties( cmtk::CommandLine::PROPS_IMAGE | cmtk::CommandLine::PROPS_LABELS );
     cl.EndGroup();
 
@@ -129,11 +131,29 @@ main( const int argc, const char* argv[] )
     refIndexLookup[refIndexValue[i].m_Index] = i;
     }
 
-  std::vector<size_t> lookup( nPixelsRef );
   const double factor = double( nPixelsFlt ) / double( nPixelsRef );
-  for ( size_t iRef = 0; iRef < nPixelsRef; ++iRef )
+  std::vector<size_t> lookup( nPixelsRef );
+
+  if ( collapse )
     {
-    lookup[iRef] = fltIndexValue[static_cast<size_t>(refIndexLookup[iRef]*factor)].m_Index;
+    for ( size_t iRef = 0; iRef < nPixelsRef; ++iRef )
+      {
+      const size_t rangeStart = iRef;
+      for ( const double rangeValue = refIndexValue[iRef].m_Value; (iRef < nPixelsRef) && (refIndexValue[iRef].m_Value == rangeValue); ++iRef );
+      
+      const size_t translation = fltIndexValue[static_cast<size_t>( 0.5 * factor * (rangeStart + iRef))].m_Index;
+      for ( size_t ii = rangeStart; ii < iRef; ++ii )
+	{
+	lookup[refIndexValue[ii].m_Index] = translation;
+	}
+      }
+    }
+  else
+    {
+    for ( size_t iRef = 0; iRef < nPixelsRef; ++iRef )
+      {
+      lookup[iRef] = fltIndexValue[static_cast<size_t>(refIndexLookup[iRef]*factor)].m_Index;
+      }
     }
   
   for ( size_t iRef = 0; iRef < nPixelsRef; ++iRef )
