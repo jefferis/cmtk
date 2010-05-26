ad42a400ecd15a3dbfa7c49b84b43bfd2a37c987
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-05-26 21:27:05 +0000

    FIX: added missing header (thanks to Hans Johnson for reporting this)
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@1757 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/System/cmtkMemory.h
+++ core/libs/System/cmtkMemory.h
@@ -1,7 +1,8 @@
 /*
 //
 //  Copyright 1997-2009 Torsten Rohlfing
-//  Copyright 2004-2009 SRI International
+//
+//  Copyright 2004-2010 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
@@ -33,6 +34,8 @@
 #define __cmtkMemory_h_included_
 
 #include <cmtkconfig.h>
+
+#include <cstdio> // thanks to Hans Johnson for pointing this out
 
 #ifdef HAVE_MALLOC_H
 #  include <malloc.h>
