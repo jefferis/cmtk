d42bfb96d1870f7ac89eff4fd740faccfeff7bcf
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-06-18 20:52:28 +0000

    REF: eliminate smart pointer use in thread function (Issue #4940)
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@1854 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/Registration/cmtkParallelElasticFunctional.h
+++ core/libs/Registration/cmtkParallelElasticFunctional.h
@@ -156,7 +156,7 @@ public:
    *@param voi Volume-of-Influence for the parameter under consideration.
    *@return The metric after recomputation over the given volume-of-influence.
    */
-  typename Self::ReturnType EvaluateIncremental( const W* warp, VM *const localMetric, const DataGrid::RegionType& voi, Vector3D *const vectorCache ) 
+  typename Self::ReturnType EvaluateIncremental( const W& warp, VM *const localMetric, const DataGrid::RegionType& voi, Vector3D *const vectorCache ) 
   {
     Vector3D *pVec;
     int pX, pY, pZ, offset, r;
@@ -174,7 +174,7 @@ public:
       for ( pY = voi.From()[1]; pY<voi.To()[1]; ++pY ) 
 	{
 	pVec = vectorCache;
-	warp->GetTransformedGridSequence( voi.To()[0]-voi.From()[0], pVec, voi.From()[0], pY, pZ );
+	warp.GetTransformedGridSequence( voi.To()[0]-voi.From()[0], pVec, voi.From()[0], pY, pZ );
 	for ( pX = voi.From()[0]; pX<voi.To()[0]; ++pX, ++r, ++pVec ) 
 	  {
 	  // Remove this sample from incremental metric according to "ground warp" image.
@@ -320,12 +320,12 @@ private:
     
     Self *me = info->thisObject;
 
-    SmartPointer<W>& myWarp = me->ThreadWarp[threadIdx];
-    myWarp->SetParamVector( *info->Parameters );
+    W& myWarp = *(me->ThreadWarp[threadIdx]);
+    myWarp.SetParamVector( *info->Parameters );
     
     VM* threadMetric = me->TaskMetric[threadIdx];
     Vector3D *vectorCache = me->ThreadVectorCache[threadIdx];
-    Types::Coordinate *p = myWarp->m_Parameters;
+    Types::Coordinate *p = myWarp.m_Parameters;
     
     Types::Coordinate pOld;
     double upper, lower;
@@ -385,7 +385,7 @@ private:
     typename Self::EvaluateCompleteTaskInfo *info = static_cast<typename Self::EvaluateCompleteTaskInfo*>( arg );
     
     Self *me = info->thisObject;
-    const W *warp = me->ThreadWarp[0];
+    const W& warp = *(me->ThreadWarp[0]);
     VM* threadMetric = me->TaskMetric[threadIdx];
     Vector3D *vectorCache = me->ThreadVectorCache[threadIdx];
     
@@ -411,7 +411,7 @@ private:
       {
       for ( pY = pYfrom; (pY < me->DimsY) && rowsToDo; pYfrom = 0, ++pY, --rowsToDo ) 
 	{
-	warp->GetTransformedGridSequence( me->DimsX, vectorCache, 0, pY, pZ );
+	warp.GetTransformedGridSequence( me->DimsX, vectorCache, 0, pY, pZ );
 	pVec = vectorCache;
 	for ( pX = 0; pX<me->DimsX; ++pX, ++r, ++pVec ) 
 	  {
