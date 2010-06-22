1be1351b551e934dfff33976e380c8cc8dfcac5c
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-06-22 22:54:02 +0000

    ADD: first test for GPU libary
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@1879 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/testing/libs/GPU/libGPUTests.cxx
@@ -0,0 +1,84 @@
+/*
+//
+//  Copyright 2004-2010 SRI International
+//
+//  Copyright 1997-2009 Torsten Rohlfing
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#include <iostream>
+#include <cstring>
+
+#include "cmtkDeviceMemoryCUDATests.txx"
+
+/** Set up table of test names and function pointers */
+typedef int (*testFuncPtr)();
+
+typedef 
+struct __testNameAndFunctionPointer
+{
+  const char* name;
+  const testFuncPtr func;
+} testNameAndFunctionPointer;
+
+const testNameAndFunctionPointer testTable[] =
+{
+  { "DeviceMemoryCUDA",               &testDeviceMemoryCUDA },
+  { NULL, NULL }
+};
+
+int
+main( const int argc, const char* argv[] )
+{
+  int testNumber = -1;
+  // is test name given on command line?
+  if ( argc < 2 )
+    {
+    // no: ask user in dialog mode
+    for ( size_t i = 0; testTable[i].name; ++i )
+      {
+      std::cout << i << ". " << testTable[i].name << std::endl;
+      }
+    std::cout << "Run test number: ";
+    std::cin >> testNumber;
+    }
+  else
+    {
+    // batch mode: find test by name given on command line
+    for ( size_t i = 0; testTable[i].name; ++i )
+      {
+      if ( !std::strcmp( argv[1], testTable[i].name ) )
+	testNumber = i;
+      }
+    }
+
+  // run test, or return error if none found
+  if ( testNumber < 0 )
+    return 2;
+  else
+    return testTable[testNumber].func();
+}
