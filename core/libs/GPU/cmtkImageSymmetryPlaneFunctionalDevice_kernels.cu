adce8c6328189c3fee19073c0103008e09c3ba99
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-07-23 23:06:51 +0000

    ADD: computed squared difference at pixel
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@2056 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/GPU/cmtkImageSymmetryPlaneFunctionalDevice_kernels.cu
+++ core/libs/GPU/cmtkImageSymmetryPlaneFunctionalDevice_kernels.cu
@@ -36,8 +36,31 @@
 
 /// Texture reference to volume data.
 texture<float, 3, cudaReadModeElementType> texRef;
+texture<float, 3, cudaReadModeElementType> texRefX;
 
 __constant__ float deviceAxesTNL[16384];
+
+__global__
+void cmtkImageSymmetryPlaneFunctionalDeviceEvaluateKernel( const int dims0, const int dims1, const int dims2 )
+{
+  const int x = threadIdx.x;
+  const int y = threadIdx.y;
+  const int z = blockIdx.x;
+
+  const int offsetX = 3 * x;
+  const int offsetY = 3 * (dims0 + y);
+  const int offsetZ = 3 * (dims0 + dims1 + z);
+
+  const float xX = deviceAxesTNL[offsetX] + deviceAxesTNL[offsetY] + deviceAxesTNL[offsetZ];
+  const float yX = deviceAxesTNL[offsetX+1] + deviceAxesTNL[offsetY+1] + deviceAxesTNL[offsetZ+1];
+  const float zX = deviceAxesTNL[offsetX+2] + deviceAxesTNL[offsetY+2] + deviceAxesTNL[offsetZ+2];
+
+  const float data = tex3D( texRef, x, y, z );
+  const float dataX = tex3D( texRefX, xX, yX, zX );
+
+  const float diff = data-dataX;
+  const float sq = diff*diff;
+}
 
 float
 cmtkImageSymmetryPlaneFunctionalDeviceEvaluate( const int* dims3, void* array, const float* axesTNL )
@@ -59,6 +82,27 @@ cmtkImageSymmetryPlaneFunctionalDeviceEvaluate( const int* dims3, void* array, c
   
   // Bind the array to the texture reference 
   cudaBindTextureToArray( texRef, (struct cudaArray*) array, channelDesc );
+
+  // Set texture parameters
+  texRefX.addressMode[0] = cudaAddressModeClamp;
+  texRefX.addressMode[1] = cudaAddressModeClamp;
+  texRefX.addressMode[2] = cudaAddressModeClamp;
+  texRefX.filterMode = cudaFilterModePoint; 
+  texRefX.normalized = false; 
+
+  cudaBindTextureToArray( texRefX, (struct cudaArray*) array, channelDesc );
+
+  dim3 dimBlock( dims3[0], dims3[1], 1 );
+  dim3 dimGrid( dims3[2], 1 );
+  
+  cmtkImageSymmetryPlaneFunctionalDeviceEvaluateKernel<<<dimGrid,dimBlock>>>( dims3[0], dims3[1], dims3[2] );
+
+  const cudaError_t kernelError = cudaGetLastError();
+  if ( kernelError != cudaSuccess )
+    {
+      fprintf( stderr, "ERROR: CUDA kernel failed with error %s\n",cudaGetErrorString( kernelError ) );
+      exit( 1 );      
+    }
 
   cudaUnbindTexture( texRef );
 
