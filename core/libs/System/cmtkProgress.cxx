9fa3ee41549c4978501bff8933aefa537ae33a2e
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-08-23 21:46:46 +0000

    OBS: cpp definition "CMTK_USE_OPENMP" no longer needed
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@2279 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/System/cmtkProgress.cxx
+++ core/libs/System/cmtkProgress.cxx
@@ -1,7 +1,8 @@
 /*
 //
 //  Copyright 1997-2009 Torsten Rohlfing
-//  Copyright 2004-2009 SRI International
+//
+//  Copyright 2004-2010 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
@@ -31,10 +32,6 @@
 
 #include "cmtkProgress.h"
 #include "System/cmtkConsole.h"
-
-#ifdef CMTK_USE_OPENMP
-#  include <omp.h>
-#endif
 
 namespace
 cmtk
--- core/libs/System/cmtkProgressConsole.cxx
+++ core/libs/System/cmtkProgressConsole.cxx
@@ -1,7 +1,8 @@
 /*
 //
 //  Copyright 1997-2009 Torsten Rohlfing
-//  Copyright 2004-2009 SRI International
+//
+//  Copyright 2004-2010 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
@@ -33,10 +34,6 @@
 
 #include "System/cmtkConsole.h"
 #include "System/cmtkTimers.h"
-
-#ifdef CMTK_USE_OPENMP
-#  include <omp.h>
-#endif
 
 #include <stdlib.h>
 
