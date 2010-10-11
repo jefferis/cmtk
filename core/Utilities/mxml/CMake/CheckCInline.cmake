9b7d64c4aab700ae90a77822e128907c7f8cf6a3
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2010-10-11 20:50:16 +0000

    FIX: path needed updating
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@2434 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/Utilities/mxml/CMake/CheckCInline.cmake
+++ core/Utilities/mxml/CMake/CheckCInline.cmake
@@ -4,7 +4,7 @@
 # Inspired from /usr/share/autoconf/autoconf/c.m4
 FOREACH(KEYWORD "inline" "__inline__" "__inline")
    IF(NOT DEFINED C_INLINE)
-     TRY_COMPILE(C_HAS_KEYWORD "${CMAKE_CURRENT_BINARY_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/CMakeFiles/CheckCInline.c" COMPILE_DEFINITIONS "-Dinline=${KEYWORD}")
+     TRY_COMPILE(C_HAS_KEYWORD "${CMAKE_CURRENT_BINARY_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/CMake/CheckCInline.c" COMPILE_DEFINITIONS "-Dinline=${KEYWORD}")
      IF(C_HAS_KEYWORD)
        SET(C_INLINE ${KEYWORD} CACHE STRING "C compiler keyword for inline functions")
        MARK_AS_ADVANCED(C_INLINE)
