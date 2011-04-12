6e7444632d280154b8e366d8719d03d2b2fdd39c
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2011-04-12 19:26:00 +0000

    FIX: recursive function call
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@3132 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/Base/cmtkMathFunctionWrappers.h
+++ core/libs/Base/cmtkMathFunctionWrappers.h
@@ -2,7 +2,7 @@
 //
 //  Copyright 1997-2009 Torsten Rohlfing
 //
-//  Copyright 2004-2010 SRI International
+//  Copyright 2004-2011 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
@@ -59,7 +59,7 @@ Log( const double x )
 double
 Exp( const double x )
 {
-  return Exp( x );
+  return exp( x );
 }
 
 /// Square root function.
