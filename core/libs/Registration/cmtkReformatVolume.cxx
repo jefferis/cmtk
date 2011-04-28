403e1d7ba595935d38cac02ddb276e7ee1a1ba02
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2011-04-28 21:52:13 +0000

    ADD: use floating image padding flag and value for reformatted output image
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@3190 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/Registration/cmtkReformatVolume.cxx
+++ core/libs/Registration/cmtkReformatVolume.cxx
@@ -2,7 +2,7 @@
 //
 //  Copyright 1997-2009 Torsten Rohlfing
 //
-//  Copyright 2004-2010 SRI International
+//  Copyright 2004-2011 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
@@ -116,7 +116,16 @@ ReformatVolume::PlainReformat()
     
     TypedArray::SmartPtr targetData( TypedArray::Create( FloatingVolume->GetData()->GetType(), targetVolume->GetNumberOfPixels() ) );
     if ( this->m_UsePaddingValue )
+      {
       targetData->SetPaddingValue( this->m_PaddingValue );
+      }
+    else
+      {
+      if ( FloatingVolume->GetData()->GetPaddingFlag() )
+	{
+	targetData->SetPaddingValue( FloatingVolume->GetData()->GetPaddingValue() );
+	}
+      }
     
     UniformVolumeInterpolatorBase::SmartPtr interpolator( this->CreateInterpolator( this->FloatingVolume ) );
     Vector3D pFlt;
