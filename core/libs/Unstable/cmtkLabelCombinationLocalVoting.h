a5d4f2256da32dc968f5ea8c00d1ad681e6e71bc
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2011-08-01 22:10:47 +0000

    ADD: skeleton local voting class
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@3316 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/libs/Unstable/cmtkLabelCombinationLocalVoting.h
@@ -0,0 +1,77 @@
+/*
+//
+//  Copyright 1997-2009 Torsten Rohlfing
+//
+//  Copyright 2004-2011 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#ifndef __cmtkLabelCombinationLocalVoting_h_included_
+#define __cmtkLabelCombinationLocalVoting_h_included_
+
+#include <cmtkconfig.h>
+
+#include <System/cmtkSmartPtr.h>
+#include <System/cmtkSmartConstPtr.h>
+
+#include <Base/cmtkUniformVolume.h>
+
+#include <vector>
+
+namespace
+cmtk
+{
+
+/** \addtogroup Segmentation */
+//@{
+
+/** Segmentation combination by locally-weighted label voting.
+ *\attention All labels must be between 0 and 255.
+ */
+class LabelCombinationLocalVoting
+{
+public:
+  /// Constructor: compute label combination.
+  LabelCombinationLocalVoting( const UniformVolume::SmartConstPtr& targetImage ) : m_TargetImage( targetImage ) {}
+  
+  /// Add an atlas (pair of reformatted, target-matched intensity image and label map).
+  void AddAtlas( UniformVolume::SmartConstPtr& image, UniformVolume::SmartConstPtr& atlas );
+  
+private:
+  /// The target image.
+  UniformVolume::SmartConstPtr m_TargetImage;
+  
+  /// Vector of target-matched atlas images.
+  std::vector<UniformVolume::SmartConstPtr> m_AtlasImages;
+
+  /// Vector of target-matched atlas label maps.
+  std::vector<UniformVolume::SmartConstPtr> m_AtlasLabels;
+};
+
+} // namespace cmtk
+
+#endif // #ifndef __cmtkLabelCombinationLocalVoting_h_included_
