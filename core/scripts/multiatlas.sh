a03a84eb95432918086fa72e59d7e21af2d686b6
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2011-08-09 23:37:39 +0000

    ADD: multiatlas.sh script
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@3349 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/scripts/multiatlas.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+TARGET=$1
+shift
+
+ATLAS_I=""
+ATLAS_L=""
+while [ "$1" != "" ]; do
+    ATLAS_I="${ATLAS_I} $1"
+    ATLAS_L="${ATLAS_L} $2"
+    shift 2
+done
+
+echo ${ATLAS_I}
+echo ${ATLAS_L}
+
+tmpdir=`mktemp -d`
+echo ${tmpdir}
+
+idx=0
+for atlas in ${ATLAS_I}; do
+    xfm=${tmpdir}/${idx}.xfm
+    cmtk registrationx --echo --init com --auto-multi-levels 5 --omit-original-data --delta-f-threshold 0.01 --dofs 6,9 --ncc --pad-ref 0 --symmetric -o ${xfm} ${TARGET} ${atlas}
+    idx=`expr ${idx} + 1`
+done
+
+idx=0
+for atlas in ${ATLAS_I}; do
+    xfm=${tmpdir}/${idx}.xfm
+    ffd=${tmpdir}/${idx}.ffd
+    cmtk warpx --echo --grid-spacing 80 --grid-refine 3 --min-stepsize 0.25 --max-stepsize 16 --smoothness-constraint-weight 1e-1 --omit-original-data --delta-f-threshold 0.01 --fast --nmi -o ${ffd} --initial ${xfm} ${TARGET} ${atlas}
+    idx=`expr ${idx} + 1`
+done
+
+idx=0
+for atlas in ${ATLAS_I}; do
+    ffd=${tmpdir}/${idx}.ffd
+    out=${tmpdir}/${idx}_i.nii
+    cmtk reformatx --floating ${atlas} --cubic -o ${out} ${TARGET} ${ffd}
+    idx=`expr ${idx} + 1`
+done
+
+lvote_inputs=""
+
+idx=0
+for atlas in ${ATLAS_L}; do
+    ffd=${tmpdir}/${idx}.ffd
+    out=${tmpdir}/${idx}_l.nii
+    cmtk reformatx --floating ${atlas} --pv -o ${out} ${TARGET} ${ffd}
+
+    lvote_inputs="${lvote_inputs} ${tmpdir}/${idx}_i.nii ${tmpdir}/${idx}_l.nii"
+    
+    idx=`expr ${idx} + 1`
+done
+
+bin/lvote --echo -o output.nii --patch-radius 5 ${TARGET} ${lvote_inputs}
