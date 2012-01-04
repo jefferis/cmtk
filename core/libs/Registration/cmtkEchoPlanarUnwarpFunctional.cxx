55ee8e0b316724f2ba2c8c3c508658984f17a23b
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2012-01-04 21:45:34 +0000

    FIX: sign
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@3687 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/Registration/cmtkEchoPlanarUnwarpFunctional.cxx
+++ core/libs/Registration/cmtkEchoPlanarUnwarpFunctional.cxx
@@ -349,11 +349,11 @@ cmtk::EchoPlanarUnwarpFunctional
       const ap::real_value_type jacF = 1 + x( ofs ) - x( ofs - sourceImage.m_GridIncrements[phaseEncodeDirection] );
       const ap::real_value_type jacR = 1 - x( ofs ) + x( ofs - sourceImage.m_GridIncrements[phaseEncodeDirection] );
       
-      fold -= ( log( jacF ) + log( jacR ) );
+      fold += ( 1.0 / (jacF*jacF) + 1.0 / (jacR*jacR) );
       
       // increment relevant gradient elements
-      g( ofs ) += lambda3 * (1.0 / jacF - 1.0 / jacR) / insideRegionSize;
-      g( ofs - sourceImage.m_GridIncrements[phaseEncodeDirection] ) -= lambda3 * (1.0 / jacF - 1.0 / jacR) / insideRegionSize;
+      g( ofs ) -= 2 * lambda3 * ( 1.0 / (jacF * jacF * jacF * insideRegionSize) - 1.0 / (jacR * jacR * jacR * insideRegionSize) ) ;
+      g( ofs - sourceImage.m_GridIncrements[phaseEncodeDirection] ) += 2 * lambda3 * ( 1.0 / (jacF * jacF * jacF * insideRegionSize) - 1.0 / (jacR * jacR * jacR * insideRegionSize) );
       }
     
     f += lambda3 * (fold /= insideRegionSize);
