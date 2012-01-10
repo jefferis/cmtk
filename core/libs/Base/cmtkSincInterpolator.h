8abb0cb45370d0a8d9673f58cb5a6b611a6be9f8
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2012-01-10 20:59:36 +0000

    STY
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@3709 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/Base/cmtkSincInterpolator.h
+++ core/libs/Base/cmtkSincInterpolator.h
@@ -1,7 +1,8 @@
 /*
 //
 //  Copyright 1997-2009 Torsten Rohlfing
-//  Copyright 2004-2009 SRI International
+//
+//  Copyright 2004-2012 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
