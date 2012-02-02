cd215d7161f4d0af5ac41e917943721769b0d8e7
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2012-02-02 23:38:48 +0000

    DOC: removed doxygen bracketing
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@3810 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/System/cmtkConsole.cxx
+++ core/libs/System/cmtkConsole.cxx
@@ -2,7 +2,7 @@
 //
 //  Copyright 1997-2009 Torsten Rohlfing
 //
-//  Copyright 2004-2011 SRI International
+//  Copyright 2004-2012 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
@@ -52,8 +52,6 @@ namespace
 cmtk
 {
 
-/** \addtogroup System */
-//@{
 Console StdErr( &std::cerr );
 Console StdOut( &std::cout );
 Console StdNull( NULL );
