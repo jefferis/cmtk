cbed97b92d87e839599c519bf0d2bff5652e0480
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2012-02-14 20:40:09 +0000

    ADD: skeleton for fit_affine_xform tool and FitAffineToWarpXform class
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@3860 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/apps/fit_affine_xform.cxx
@@ -0,0 +1,77 @@
+/*
+//
+//  Copyright 1997-2011 Torsten Rohlfing
+//
+//  Copyright 2004-2012 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#include <cmtkconfig.h>
+
+#include <System/cmtkConsole.h>
+#include <System/cmtkCommandLine.h>
+#include <System/cmtkProgress.h>
+
+#include <Base/cmtkWarpXform.h>
+#include <Base/cmtkAffineXform.h>
+#include <Base/cmtkFitAffineToWarpXform.h>
+
+#include <IO/cmtkXformIO.h>
+
+const char* InputPath = NULL;
+const char *OutputPath = NULL;
+
+int
+doMain ( const int argc, const char *argv[] ) 
+{
+  try
+    {
+    cmtk::CommandLine cl;
+    cl.SetProgramInfo( cmtk::CommandLine::PRG_TITLE, "Fit Affine Transformation to Nonrigid Transformation" );
+    cl.SetProgramInfo( cmtk::CommandLine::PRG_DESCR, "Fit a linear affine transformation to a nonrigid transformation, either a B-spline free-form deformation or a non-parametric deformation field." );
+    
+    typedef cmtk::CommandLine::Key Key;
+    cl.AddParameter( &InputPath, "InputDField", "Input nonridid transformation." )->SetProperties( cmtk::CommandLine::PROPS_XFORM );  
+    cl.AddParameter( &OutputPath, "OutputXform", "Path for output fitted affine transformation." )->SetProperties( cmtk::CommandLine::PROPS_XFORM | cmtk::CommandLine::PROPS_OUTPUT );
+    
+    cl.Parse( argc, argv );
+    }
+  catch ( const cmtk::CommandLine::Exception& e )
+    {
+    cmtk::StdErr << e;
+    throw cmtk::ExitException( 1 );
+    }
+
+  cmtk::WarpXform::SmartPtr warpXform = cmtk::WarpXform::SmartPtr::DynamicCastFrom( cmtk::XformIO::Read( InputPath ) );
+  
+  cmtk::FitAffineToWarpXform fitAffine( warpXform );
+  cmtk::XformIO::Write( fitAffine.Fit(), OutputPath );
+  
+  return 0;
+}
+
+#include "cmtkSafeMain"
---
+++ core/libs/Base/cmtkFitAffineToWarpXform.h
@@ -0,0 +1,66 @@
+/*
+//
+//  Copyright 2012 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#ifndef __cmtkFitAffineToWarpXform_h_included_
+#define __cmtkFitAffineToWarpXform_h_included_
+
+#include <cmtkconfig.h>
+
+#include <Base/cmtkWarpXform.h>
+
+namespace
+cmtk
+{
+
+/** \addtogroup Base */
+//@{
+
+/** Fit affine transformation to nonrigid (B-spline or deformation field) transformation.
+ */
+class FitAffineToWarpXform
+{
+public:
+  /// This class.
+  typedef FitAffineToWarpXform Self;
+
+  /// Constructor.
+  FitAffineToWarpXform( WarpXform::SmartConstPtr warp );
+
+  /// Fit affine transformation.
+  AffineXform::SmartPtr Fit();
+  
+private:
+  /// Input nonrigid warp transformation.
+  WarpXform::SmartConstPtr m_WarpXform;
+};
+
+} // namespace
+
+#endif // #ifndef __cmtkFitAffineToWarpXform_h_included_
