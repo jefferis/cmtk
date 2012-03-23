b054a636b71a75ac0b11f8af25d68a34f916819e
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2012-03-23 22:07:29 +0000

    DOC
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@4079 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/apps/glm.cxx
+++ core/apps/glm.cxx
@@ -2,7 +2,7 @@
 //
 //  Copyright 1997-2010 Torsten Rohlfing
 //
-//  Copyright 2004-2011 SRI International
+//  Copyright 2004-2012 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
