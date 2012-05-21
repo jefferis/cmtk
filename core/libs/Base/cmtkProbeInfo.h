30509468384da05a67fc1e0e1643ea08b64bd0d8
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2012-05-21 22:34:17 +0000

    OBS: removed unused members of ProbeInfor
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@4353 42a5c34f-2066-0410-bec5-ba365beb4995

--- core/libs/Base/cmtkProbeInfo.h
+++ core/libs/Base/cmtkProbeInfo.h
@@ -2,7 +2,7 @@
 //
 //  Copyright 1997-2009 Torsten Rohlfing
 //
-//  Copyright 2004-2010 SRI International
+//  Copyright 2004-2012 SRI International
 //
 //  This file is part of the Computational Morphometry Toolkit.
 //
@@ -86,97 +86,6 @@ public:
 					     Offsets[4]*(Offsets[0]*Values[6]+Offsets[3]*Values[7]) ) );
   }
 
-  /** Return data value at desired location using only values above threshold.
-   */
-  Types::DataItem GetValueTrilinearAbove ( const Types::DataItem threshold ) const 
-  {
-    Types::DataItem result = 0;
-    for ( int idx=0; idx<8; ++idx ) 
-      {
-      if ( Values[idx] >= threshold )
-	result += static_cast<Types::DataItem>( GetWeight( idx ) * Values[idx] );
-      }
-    return result;
-  }
-
-  /** Return data value at desired location using only values below threshold.
-   */
-  Types::DataItem GetValueTrilinearBelow ( const Types::DataItem threshold ) const 
-  {
-    Types::DataItem result = 0;
-    for ( int idx=0; idx<8; ++idx ) {
-      if ( Values[idx] < threshold )
-	result += static_cast<Types::DataItem>( GetWeight( idx ) * Values[idx] );
-    }
-    return result;
-  }
-
-  /** Return number of values above (or equal to) threshold.
-   */
-  byte GetNumberOfValuesAbove ( const Types::DataItem threshold ) const 
-  {
-    byte result = 0;
-    for ( int idx=0; idx<8; ++idx )
-      if ( Values[idx] >= threshold )
-	++result;
-    return result;
-  }
-
-  /** Return number of values below threshold.
-   */
-  byte GetNumberOfValuesBelow ( const Types::DataItem threshold ) const 
-  {
-    byte result = 0;
-    for ( int idx=0; idx<8; ++idx )
-      if ( Values[idx] < threshold )
-	++result;
-    return result;
-  }
-
-  /// Return relative weight of given index.
-  Types::Coordinate GetWeight( const int index ) const 
-  {
-    switch ( index ) 
-      {
-      case 0: return Offsets[2] * Offsets[1] * Offsets[0];
-      case 1: return Offsets[2] * Offsets[1] * Offsets[3];
-      case 2: return Offsets[2] * Offsets[4] * Offsets[0];
-      case 3: return Offsets[2] * Offsets[4] * Offsets[3];
-      case 4: return Offsets[5] * Offsets[1] * Offsets[0];
-      case 5: return Offsets[5] * Offsets[1] * Offsets[3];
-      case 6: return Offsets[5] * Offsets[4] * Offsets[0];
-      case 7: return Offsets[5] * Offsets[4] * Offsets[3];
-      }
-    return 0;
-  }
-
-  /** Return partial derivatives of node data w.r.t. grid dimensions.
-   * The data on the cube faces is used for a finite-difference approximation
-   * of the first-order derivatives w.r.t. the grid dimensions x, y, and z.
-   *\param d The object of type Vector3D in which the result is to be stored.
-   *\return A reference to the destination parameter.
-   */
-  Vector3D& GetPartialDerivatives ( Vector3D& d ) const 
-  {
-    d[0] = static_cast<Types::Coordinate>( Deltas[0] * 
-					   ( Offsets[2]*(Offsets[1]*Values[1]+Offsets[4]*Values[3])+
-					     Offsets[5]*(Offsets[1]*Values[5]+Offsets[4]*Values[7])-
-					     Offsets[2]*(Offsets[1]*Values[0]+Offsets[4]*Values[2])-
-					     Offsets[5]*(Offsets[1]*Values[4]+Offsets[4]*Values[6])) );
-    
-    d[1] = static_cast<Types::Coordinate>( Deltas[1] * 
-					   ( Offsets[2]*(Offsets[0]*Values[2]+Offsets[3]*Values[3])+
-					     Offsets[5]*(Offsets[0]*Values[6]+Offsets[3]*Values[7])-
-					     Offsets[2]*(Offsets[0]*Values[0]+Offsets[3]*Values[1])-
-					     Offsets[5]*(Offsets[0]*Values[4]+Offsets[3]*Values[5])) );
-    
-    d[2] = static_cast<Types::Coordinate>( Deltas[2] *
-					   ( Offsets[1]*(Offsets[0]*Values[4]+Offsets[3]*Values[5])+
-					     Offsets[4]*(Offsets[0]*Values[6]+Offsets[3]*Values[7])-
-					     Offsets[1]*(Offsets[0]*Values[0]+Offsets[3]*Values[1])-
-					     Offsets[4]*(Offsets[0]*Values[2]+Offsets[3]*Values[3])) );
-    return d;
-  }
 };
 
 //@}
