29562de3b48bfddc34f3eb9f58eda42d4aa30753
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2012-05-30 21:33:32 +0000

    ADD: class to fit spline to transformation list
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@4385 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/libs/Base/cmtkFitSplineWarpToXformList.cxx
@@ -0,0 +1,205 @@
+/*
+//
+//  Copyright 2012 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#include "cmtkFitSplineWarpToXformList.h"
+
+#include <Base/cmtkRegionIndexIterator.h>
+
+#include <System/cmtkDebugOutput.h>
+
+cmtk::FitSplineWarpToXformList::FitSplineWarpToXformList( const UniformVolume& sampleGrid, const XformList& xformList, const bool absolute )
+  : m_XformField( sampleGrid )
+{
+  this->m_XformValidAt.resize( this->m_XformField.GetNumberOfPixels() );
+  std::fill( this->m_XformValidAt.begin(), this->m_XformValidAt.end(), true );
+
+  size_t ofs = 0;
+  for ( RegionIndexIterator<DataGrid::RegionType> voxelIt( this->m_XformField.GetWholeImageRegion() ); voxelIt != voxelIt.end(); ++voxelIt, ++ofs )
+    {
+    const Xform::SpaceVectorType v = this->m_XformField.GetGridLocation( voxelIt.Index() );
+    
+    Xform::SpaceVectorType u = v;
+    if ( xformList.ApplyInPlace( u ) )
+      {
+      if ( !absolute )
+	u -= v;
+      this->m_XformField[ofs] = u;
+      }
+    else
+      {
+      this->m_XformValidAt[ofs] = false;
+      }
+    }
+}
+
+void
+cmtk::FitSplineWarpToXformList::ComputeResiduals( const SplineWarpXform& splineWarp )
+{
+  const DataGrid::IndexType dims = this->m_XformField.m_Dims;
+
+  this->m_Residuals.resize( dims.Product() );
+
+#pragma omp parallel for
+  for ( int z = 0; z < dims[2]; ++z )
+    {
+    size_t ofs = z * dims[0] * dims[1];
+
+    for ( int y = 0; y < dims[1]; ++y )
+      {
+      for ( int x = 0; x < dims[0]; ++x, ++ofs )
+	{
+	this->m_Residuals[ofs] = this->m_XformField[ofs] - splineWarp.GetTransformedGrid( x, y, z );
+	}
+      }
+    }
+}
+
+cmtk::SplineWarpXform::SmartPtr 
+cmtk::FitSplineWarpToXformList::Fit( const SplineWarpXform::ControlPointIndexType& finalDims, const int nLevels, const AffineXform* affineXform )
+{
+  // we may have to adjust nLevels downwards
+  int numberOfLevels = nLevels;
+  
+  SplineWarpXform::ControlPointIndexType initialDims = finalDims;
+  for ( int level = 1; level < numberOfLevels; ++level )
+    {
+    if ( (initialDims[0]&1) && (initialDims[1]&1) && (initialDims[2]&1) && // check that all dims are still odd numbers
+	 (initialDims.MinValue()>4) ) // check that at least 4 control points will be left in each dimension
+      {
+      // apply the inverse of the refinement formula used in SplineWarpXform::Refine()
+      initialDims.AddScalar( +3 );
+      initialDims /= 2;
+      }
+    else
+      {
+      numberOfLevels = level;
+
+      DebugOutput(2) << "INFO: adjusted number of levels to " << numberOfLevels << " from " << nLevels << " to ensure sufficient number of control points\n";
+      }
+    }
+
+  // initialize B-spline transformation
+  AffineXform::SmartPtr initialAffine( affineXform ? new AffineXform( *affineXform ) : new AffineXform );
+  SplineWarpXform* splineWarp = new SplineWarpXform( this->m_XformField.m_Size, initialDims, CoordinateVector::SmartPtr::Null(), initialAffine );
+  
+  this->FitSpline( *splineWarp, numberOfLevels );
+  
+  return cmtk::SplineWarpXform::SmartPtr( splineWarp );
+}
+
+cmtk::SplineWarpXform::SmartPtr 
+cmtk::FitSplineWarpToXformList::Fit( const Types::Coordinate finalSpacing, const int nLevels, const AffineXform* affineXform )
+{
+  // compute the start spacing of multi-level approximation by doubling final spacing until user-defined initial spacing is exceeded.
+  Types::Coordinate spacing = finalSpacing * (1 << (nLevels-1));
+
+  // initialize B-spline transformation
+  AffineXform::SmartPtr initialAffine( affineXform ? new AffineXform( *affineXform ) : new AffineXform );
+  SplineWarpXform* splineWarp = new SplineWarpXform( this->m_XformField.m_Size, spacing, initialAffine );
+  
+  this->FitSpline( *splineWarp, nLevels );
+  
+  return cmtk::SplineWarpXform::SmartPtr( splineWarp );
+}
+
+void 
+cmtk::FitSplineWarpToXformList::FitSpline( SplineWarpXform& splineWarp, const int nLevels )
+{
+  // loop until final control point spacing
+  for ( int level = 0; level < nLevels; ++level )
+    {
+    DebugOutput( 5 ) << "Multi-resolution spline fitting level " << level+1 << " out of " << nLevels << "\n";
+    // refine control point grid unless this is first iteration
+    if ( level )
+      {
+      splineWarp.Refine();
+      }
+
+    DebugOutput( 6 ) << "  Control point grid is " << splineWarp.m_Dims[0] << "x" << splineWarp.m_Dims[1] << "x" << splineWarp.m_Dims[2] << "\n";
+
+    // compute residuals
+    splineWarp.RegisterVolume( this->m_XformField );
+    this->ComputeResiduals( splineWarp );
+    
+    // loop over all control points to compute deltas as the spline coefficients that fit current residuals
+    std::vector< FixedVector<3,Types::Coordinate> > delta( splineWarp.m_NumberOfControlPoints, FixedVector<3,Types::Coordinate>( FixedVector<3,Types::Coordinate>::Init( 0.0 ) ) );
+    std::vector< Types::Coordinate > weight( splineWarp.m_NumberOfControlPoints, 0.0 );
+    
+    for ( RegionIndexIterator<DataGrid::RegionType> voxelIt( this->m_XformField.GetWholeImageRegion() ); voxelIt != voxelIt.end(); ++voxelIt )
+      {
+      const DataGrid::IndexType voxelIdx = voxelIt.Index();
+
+      Types::Coordinate sumOfSquares = 0;
+      Types::Coordinate wklm[4][4][4], w2klm[4][4][4];
+      for ( int m = 0; m < 4; ++m )
+	{
+	for ( int l = 0; l < 4; ++l )
+	  {
+	  const Types::Coordinate wlm = splineWarp.m_GridSpline[1][4*voxelIdx[1]+l] * splineWarp.m_GridSpline[2][4*voxelIdx[2]+m];
+	  for ( int k = 0; k < 4; ++k )
+	    {
+	    sumOfSquares += (w2klm[m][l][k] = MathUtil::Square( wklm[m][l][k] = splineWarp.m_GridSpline[0][4*voxelIdx[0]+k] * wlm ) );
+	    }
+	  }
+	}
+      
+      for ( int m = 0; m < 4; ++m )
+	{ const size_t mOfs = splineWarp.m_Dims[1] * ( splineWarp.m_GridIndexes[2][voxelIdx[2]] + m );
+	for ( int l = 0; l < 4; ++l )
+	  {
+	  const size_t mlOfs = splineWarp.m_Dims[0] * ( splineWarp.m_GridIndexes[1][voxelIdx[1]] + l + mOfs );
+	  for ( int k = 0; k < 4; ++k )
+	    {
+	    const size_t cpOfs = splineWarp.m_GridIndexes[0][voxelIdx[0]] + k + mlOfs;
+	    
+	    delta[cpOfs] += w2klm[m][l][k] * wklm[m][l][k] / sumOfSquares * this->m_Residuals[this->m_XformField.GetOffsetFromIndex( voxelIdx )/3];
+	    weight[cpOfs] += w2klm[m][l][k];
+	    }
+	  }
+	}
+      }
+    
+    // apply delta
+#pragma omp parallel for
+    for ( size_t cp = 0; cp < splineWarp.m_NumberOfControlPoints; ++cp )
+      {
+      if ( weight[cp] != 0 )
+	{
+	delta[cp] /= weight[cp];
+	splineWarp.SetShiftedControlPointPositionByOffset( splineWarp.GetShiftedControlPointPositionByOffset( cp ) + delta[cp], cp );
+	}
+      else
+	{
+	// nothing to do - keep control point where it is.
+	}
+      }
+    }
+}
+
---
+++ core/libs/Base/cmtkFitSplineWarpToXformList.h
@@ -0,0 +1,101 @@
+/*
+//
+//  Copyright 2012 SRI International
+//
+//  This file is part of the Computational Morphometry Toolkit.
+//
+//  http://www.nitrc.org/projects/cmtk/
+//
+//  The Computational Morphometry Toolkit is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  The Computational Morphometry Toolkit is distributed in the hope that it
+//  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with the Computational Morphometry Toolkit.  If not, see
+//  <http://www.gnu.org/licenses/>.
+//
+//  $Revision$
+//
+//  $LastChangedDate$
+//
+//  $LastChangedBy$
+//
+*/
+
+#ifndef __cmtkFitSplineWarpToXformList_h_included_
+#define __cmtkFitSplineWarpToXformList_h_included_
+
+#include <cmtkconfig.h>
+
+#include <Base/cmtkXformList.h>
+#include <Base/cmtkSplineWarpXform.h>
+#include <Base/cmtkCubicSpline.h>
+#include <Base/cmtkRegion.h>
+#include <Base/cmtkImageTemplate.h>
+
+namespace
+cmtk
+{
+
+/** \addtogroup Base */
+//@{
+
+/** Fit B-spline-based free-form deformation to list of concatenated transformations, sampled on a discrete grid.
+ *\see This class implements the algorithm from: N. J. Tustison, B. B. Avants, and J. C. Gee, "Directly manipulated free-form deformation image registration," IEEE Transactions on Image Processing, vol. 18, no. 3, pp. 624-635, 2009;
+ * http://dx.doi.org/10.1109/TIP.2008.2010072
+ *\see The implementation itself is more closely following S. Lee, G. Wolberg, and S. Y. Shin, “Scattered data interpolation with multilevel B-splines,” IEEE Transactions on Visualization and Computer Graphics, 
+ * vol. 3, no. 3, pp. 228-244, 1997. http://dx.doi.org/10.1109/2945.620490
+ */
+class FitSplineWarpToXformList
+{
+public:
+  /// This class.
+  typedef FitSplineWarpToXformList Self;
+
+  /// Constructor.
+  FitSplineWarpToXformList( const UniformVolume& sampleGrid /*!< Discrete pixel grid where the spline transformation is sampled and residuals are minimized.*/,
+			    const XformList& xformList /*!< List of concatenated transformation that the spline transformation is fitted to.*/, 
+			    const bool absolute /*!< Flag fitting absolute transformation vs. relative deformation field */ );
+
+  /// Fit spline warp based on final grid dimensions.
+  SplineWarpXform::SmartPtr Fit( const SplineWarpXform::ControlPointIndexType& finalDims /*!< Final spline control point grid dimensions.*/, 
+				 const int nLevels /*!< Number of levels in the multi-resolution fitting.*/,
+				 const AffineXform* initialAffine = NULL /*!< Optional affine transformation to initialize the spline control points.*/ );
+
+  /// Fit spline warp based on final grid spacing.
+  SplineWarpXform::SmartPtr Fit( const Types::Coordinate finalSpacing /*!< Final control point spacing of the fitted B-spline free-form deformation*/, 
+				 const int nLevels = 1 /*!< Number of levels for optional multi-resolution fit (default: single-resolution fit)*/,
+				 const AffineXform* initialAffine = NULL /*!< Optional affine transformation to initialize the spline control points.*/  );
+  
+private:
+  /** Flag for absolute vs. relative deformation fields.
+   * If this is true, the spline is fitted to the absolute transformation defined by the deformation field.
+   * If this is false, the spline is fitted to the relative deformation field itself.
+   */
+  bool m_FitAbsolute;
+
+  /// Sampled transformation field.
+  ImageTemplate<Xform::SpaceVectorType> m_XformField;
+
+  /// Bit flags to mark pixels where the transformation is valid or not (e.g., due to failed numerical inversion).
+  std::vector<bool> m_XformValidAt;
+
+  /// Deformation field residuals, i.e., pixel-wise difference between B-spline transformation and deformation field.
+  std::vector< FixedVector<3,Types::Coordinate> > m_Residuals;
+
+  /// Compute residuals, i.e., pixel-wise difference between B-spline transformation and deformation field.
+  void ComputeResiduals( const SplineWarpXform& splineWarp );
+
+  /// Fit spline warp based on initial warp object.
+  void FitSpline( SplineWarpXform& splineWarp, const int nLevels );
+};
+
+} // namespace
+
+#endif // #ifndef __cmtkFitSplineWarpToXformList_h_included_
