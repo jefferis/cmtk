445a9a3e3ff8328b260c09c43e8ae101c6de7f79
torstenrohlfing <torstenrohlfing@42a5c34f-2066-0410-bec5-ba365beb4995>
2014-04-02 20:03:01 +0000

    FIX: our CUDA version not working with MacPorts compilers
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@5278 42a5c34f-2066-0410-bec5-ba365beb4995

--- testclient/neuro-test3/config/gcc-mp-Release.cmake
+++ testclient/neuro-test3/config/gcc-mp-Release.cmake
@@ -1,7 +1,7 @@
 ##
 ##  Copyright 1997-2011 Torsten Rohlfing
 ##
-##  Copyright 2004-2013 SRI International
+##  Copyright 2004-2014 SRI International
 ##
 ##  This file is part of the Computational Morphometry Toolkit.
 ##
@@ -63,7 +63,7 @@ CMAKE_CXX_FLAGS:STRING=-Wall -Wextra -Wshadow
 
 CMTK_USE_DCMTK:BOOL=ON
 CMTK_USE_SQLITE:BOOL=ON
-CMTK_USE_CUDA:BOOL=ON
+CMTK_USE_CUDA:BOOL=OFF
 CMTK_USE_FFTW:BOOL=ON
 CMTK_USE_QT:BOOL=ON
 
