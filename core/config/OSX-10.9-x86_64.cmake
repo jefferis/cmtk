7f18764984afaae38224d6033301a12ed8720939
jefferis <jefferis@42a5c34f-2066-0410-bec5-ba365beb4995>
2016-02-01 14:36:59 +0000

    Add OSX 10.9 config targeting 10.6
    * this is what I used to build CMTK-3.3.1-MacOSX-10.6-x86_64.dmg
    * I do not have 10.8 SDK on my machine
    
    git-svn-id: https://www.nitrc.org/svn/cmtk/trunk@5432 42a5c34f-2066-0410-bec5-ba365beb4995

---
+++ core/config/OSX-10.9-x86_64.cmake
@@ -0,0 +1,70 @@
+##
+##  Copyright 2010 Greg Jefferis
+##
+##  Copyright 2010-2012, 2014 SRI International
+##
+##  This file is part of the Computational Morphometry Toolkit.
+##
+##  http://www.nitrc.org/projects/cmtk/
+##
+##  The Computational Morphometry Toolkit is free software: you can
+##  redistribute it and/or modify it under the terms of the GNU General Public
+##  License as published by the Free Software Foundation, either version 3 of
+##  the License, or (at your option) any later version.
+##
+##  The Computational Morphometry Toolkit is distributed in the hope that it
+##  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+##  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License along
+##  with the Computational Morphometry Toolkit.  If not, see
+##  <http://www.gnu.org/licenses/>.
+##
+##  $Revision$
+##
+##  $LastChangedDate$
+##
+##  $LastChangedBy$
+##
+
+SET(CMAKE_BUILD_TYPE "Release" CACHE STRING "Build type")
+SET(CMAKE_INSTALL_PREFIX "/opt/local" CACHE PATH "Install prefix")
+SET(BUILD_SHARED_LIBS "ON")
+SET(CMTK_BUILD_SHARED "ON")
+SET(CMAKE_MACOSX_RPATH 1)
+
+# General settings
+SET(CMTK_SYSTEM_NAME "MacOSX-10.6" CACHE STRING "System name")
+SET(CMTK_SYSTEM_PROCESSOR "x86_64" CACHE STRING "System processor")
+
+
+
+# 64 bit for OS X >=10.6
+SET(CMAKE_OSX_ARCHITECTURES "x86_64" CACHE STRING "OS-X architecture")
+SET(CMAKE_OSX_DEPLOYMENT_TARGET "10.6" CACHE STRING "OS-X target")
+SET(CMAKE_OSX_SYSROOT "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" CACHE PATH "OS-X SDK")
+
+# Activate SSE support for floating point
+SET(CMAKE_CXX_FLAGS "-march=core2 -mmmx -msse -msse2 -mfpmath=sse" CACHE STRING "C++ compiler flags")
+SET(CMAKE_C_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING "C compiler flags")
+
+# CMTK config settings
+SET(CMTK_USE_SQLITE OFF CACHE BOOL "Use SQLite database")
+SET(CMTK_USE_QT OFF CACHE BOOL "Use Qt toolkit")
+SET(BUILD_GUI OFF CACHE BOOL "Build GUI applications (requires Qt)")
+SET(CMTK_USE_CUDA OFF CACHE BOOL "Use CUDA for GPU acceleration" )
+SET(CMTK_USE_LZMA OFF CACHE BOOL "Use LZMA library for decompression")
+
+# Disable OpenMP - broken on Mac
+SET(CMTK_USE_OPENMP OFF CACHE BOOL "Use OpenMP for parallelization" )
+
+# Disable FFTW, even if installed on our build system, since it requires MacPorts
+SET(CMTK_USE_FFTW OFF CACHE BOOL "Use FFTW library (required for ADNI phantom detection)")
+
+SET(BUILD_SHARED_LIBS "ON")
+SET(CMTK_BUILD_SHARED "ON")
+
+# Enable Greg's "munger" script
+SET(BUILD_CONTRIB ON CACHE BOOL "Build, install, and package third-party contributions")
+SET(BUILD_CONTRIB_MUNGER ON CACHE BOOL "Build, install, and package Greg Jefferis' munger script (requires perl)")
